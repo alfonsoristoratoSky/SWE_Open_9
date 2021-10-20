44e6e106515e4404cb83ab6827c0b390077ff72b
alfonsoristoratoSky <alfonso.ristorato@sky.uk>
2021-10-20 15:28:50 +0100

    complete project and tests

--- scooterSystem/tests/scooterApp.test.js
+++ scooterSystem/tests/scooterApp.test.js
@@ -49,11 +49,6 @@ describe('Scooter app class', () => {
         expect(() => ScooterApp.registerUser('Mark', '2005-10-10', 4444333322221111, 1022, 344)).toThrowError('You must be 18 years old to use this app')
     })
 
-    // test('insertLocation function can only show available locations', () => {
-    //     let x = ScooterApp.insertLocation('dummy location');
-    //     expect(console.log).toBeCalledWith(`We do not serve 'dummy location'`)
-    // })
-
     test('app can register an user', () => {
 
         expect(ScooterApp.users).toEqual(expect.arrayContaining(
@@ -250,51 +245,58 @@ describe('Scooter app class', () => {
         jest.clearAllTimers()
 
     })
-    test('charge a scooter', () => {
-        jest.useFakeTimers()
+    // test('charge a scooter', () => {
+    //     jest.useFakeTimers()
         
-        /**
-         * we will now return the scooter
-         * and as the battery is at 95, the ScooterApp.locationSelected.chargeScooter() method
-         * inside returnScooter(),
-         * which calls the chargingStation.chargeScooter(), will run
-         */
-        ScooterApp.insertLocation('Beach')
-        ScooterApp.returnScooter()
-        // we are now starting to charge, let's go ahead 2 seconds
-        jest.advanceTimersByTime(2000)
-        expect(scoot2.battery).toBe(97)
-        // distance travelled resets only once charge is at 100
-        expect(scoot2.distanceTravelled).toBe(0.32*5)
+    //     /**
+    //      * we will now return the scooter
+    //      * and as the battery is at 95, the ScooterApp.locationSelected.chargeScooter() method
+    //      * inside returnScooter(),
+    //      * which calls the chargingStation.chargeScooter(), will run
+    //      */
+    //     ScooterApp.insertLocation('Beach')
+    //     ScooterApp.returnScooter()
+    //     // we are now starting to charge, let's go ahead 2 seconds
+    //     jest.advanceTimersByTime(2000)
+    //     expect(scoot2.battery).toBe(97)
+    //     // distance travelled resets only once charge is at 100
+    //     expect(scoot2.distanceTravelled).toBe(0.32*5)
 
-        // let's charge to completion, 3 more seconds
-        jest.advanceTimersByTime(3000)
-        expect(scoot2.battery).toBe(100)
-        expect(scoot2.distanceTravelled).toBe(0)
-        jest.clearAllTimers()
+    //     // let's charge to completion, 3 more seconds
+    //     jest.advanceTimersByTime(3000)
+    //     expect(scoot2.battery).toBe(100)
+    //     expect(scoot2.distanceTravelled).toBe(0)
+    //     jest.clearAllTimers()
 
-    })
+    // })
     test('reportBroken(), which callMaintenance() and returnScooter() - callMainenance() also triggers repairScooter()', () => {
-        jest.useFakeTimers()
-        
+        jest.useFakeTimers()        
         /**
-         * we will now return the scooter
-         * and as the battery is at 95, the ScooterApp.locationSelected.chargeScooter() method
-         * inside returnScooter(),
-         * which calls the chargingStation.chargeScooter(), will run
+         * we will now reportBroken the scooter
+         * which will mark the scooter under maintenance, 
+         * call maintenance and trigger repairing
          */
         ScooterApp.insertLocation('Beach')
-        ScooterApp.returnScooter()
-        // we are now starting to charge, let's go ahead 2 seconds
+        ScooterApp.reportBroken()
+        // we are now starting to charge and repair at the same time, let's go ahead 2 seconds
         jest.advanceTimersByTime(2000)
         expect(scoot2.battery).toBe(97)
         // distance travelled resets only once charge is at 100
         expect(scoot2.distanceTravelled).toBe(0.32*5)
+        // scoot2 is under maintenance
+        expect(scoot2.isUnderMaintenance).toBe(true)
 
         // let's charge to completion, 3 more seconds
+        // but scooter still under maintenance, as it takes 20 secs to repair
         jest.advanceTimersByTime(3000)
         expect(scoot2.battery).toBe(100)
         expect(scoot2.distanceTravelled).toBe(0)
+        expect(scoot2.isUnderMaintenance).toBe(true)
+
+        // let's wait 15 more secs, for a total of 20 
+        // to prove that scooter is repaired
+        jest.advanceTimersByTime(15000)
+        expect(scoot2.isUnderMaintenance).toBe(false)
         jest.clearAllTimers()
 
     })
